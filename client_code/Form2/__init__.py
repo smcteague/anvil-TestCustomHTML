91bc7e1df038fd5c33cf9d57f8513f9d3a38b9c6
smcteague@yahoo.com <smcteague@yahoo.com>
2025-02-16 19:18:44 +0000

    Created Form2; Edited Form1

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import Form2Template
 from anvil import *
 import anvil.server
 import anvil.users
@@ -7,7 +7,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class Form2(Form2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
