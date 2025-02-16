d3fdd229a20a265d2b3f1e9b11f90841ee473931
smcteague@yahoo.com <smcteague@yahoo.com>
2025-02-16 20:42:04 +0000

    Edited settings, Form2 and Form1; Deleted AccountManagement, AccountManagement.ChangeEmail, AccountManagement.ChangeName, AccountManagement.DeleteAccountAlert and 8 more

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -2,9 +2,6 @@ from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
 import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 
 
 class Form1(Form1Template):
--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -2,9 +2,6 @@ from ._anvil_designer import Form2Template
 from anvil import *
 import anvil.server
 import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 
 
 class Form2(Form2Template):
